c530952a94c5b6f1e5c8f2f6e9c03116528decaf
Yann Nóbrega <yann.nicholas@gmail.com>
2025-08-21 22:54:56 -0300

    adding utils to src init

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,4 @@
-from . import functional
+from . import functional, utils
 from .oop import Calculator, Measurement, MeasurementGroup, Patient, Plotter
 
-__all__ = ["functional", "Calculator", "Measurement", "MeasurementGroup", "Patient", "Plotter"]
+__all__ = ["functional", "utils", "Calculator", "Measurement", "MeasurementGroup", "Patient", "Plotter"]
